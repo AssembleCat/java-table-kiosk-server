aab30101dbc503571638f7756240187163c2fda5
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2023-11-20 18:15:47 +0900

    [feat] project init

---
+++ src/main/kotlin/com/java/kiosk/app/JavaTableKioskServerApplication.kt
@@ -0,0 +1,11 @@
+package com.java.kiosk.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaTableKioskServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaTableKioskServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/java/kiosk/core/JavaTableKioskServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.java.kiosk.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaTableKioskServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
