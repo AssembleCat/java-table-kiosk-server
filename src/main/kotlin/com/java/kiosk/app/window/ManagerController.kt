aeeaa4bf86ae3fd6eb1f8d0b752ae8e7ede366f9
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2023-11-27 16:56:19 +0900

    [feat] 로그인 로직 Boolean 반환하도록 수정

--- src/main/kotlin/com/java/kiosk/app/window/ManagerController.kt
+++ src/main/kotlin/com/java/kiosk/app/window/ManagerController.kt
@@ -1,6 +1,5 @@
 package com.java.kiosk.app.window
 
-import com.java.kiosk.app.common.dto.response.ManagerLoginResponseDto
 import com.java.kiosk.app.core.service.core.ManagerCoreService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,7 +16,7 @@ class ManagerController(
     fun managerLogin(
         @RequestParam("email") email: String,
         @RequestParam("password") password: String
-    ): ManagerLoginResponseDto? {
+    ): Boolean {
         return managerCoreService.checkLogin(email, password)
     }
 }
